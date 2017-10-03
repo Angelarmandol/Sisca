8a841179b60027018e1e904288e712d4c0686d2b
Angel Lopez <angelarmandol@hotmail.com>
2017-10-03 14:31:03 -0700

    Rename MeinRealReal.java to Clases Java/Conexion.javaMeinRealReal.java

--- Clases Java/Conexion.javaMeinRealReal.java
+++ Clases Java/Conexion.javaMeinRealReal.java
@@ -23,12 +23,12 @@ public class MeinRealReal {
 /**
  * 
  * "Por que las mentes agudas que
- * han inventado una m·quina que haga
+ * han inventado una m√°quina que haga
  * el trabajo de 60 hombres, deben
  * ahora encontrar la forma de conservar
  * y buscar trabajo a los desplazados."
  * 
  * Robert O'Brien | Colexion cientifica de TimeLife |
- *  M·quinas | 1979 | Pagina 188
+ *  M√°quinas | 1979 | Pagina 188
  */
 }
